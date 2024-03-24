60105fa25e59cb9f610f2158d44a394c68295e3d
shreeja152 <shrija1502@gmail.com>
2024-03-25 01:45:46 +0530

    Add calculator

---
+++ Day1-Python_Basics/calculator.py
@@ -0,0 +1,15 @@
+first_number = input("First num: ")
+second_number = input("Second num: ")
+
+sum = int(first_number) + int(second_number)
+print(sum)
+
+substarction = float(first_number) - float(second_number)
+print("Ans: " + str(substarction))
+
+multiplication = int(first_number) * int(second_number)
+print(multiplication)
+
+div = int(first_number) / int(second_number)
+print(div)
+
