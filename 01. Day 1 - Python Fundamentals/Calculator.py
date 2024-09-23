8f8ba3e1badb0cf9d1833be42dd3ccded54aa504
Shreeja K Suresh <95074904+shreeja152@users.noreply.github.com>
2024-09-23 09:51:08 +0530

    Update Calculator.py

--- 01. Day 1 - Python Fundamentals/Calculator.py
+++ 01. Day 1 - Python Fundamentals/Calculator.py
@@ -1,8 +1,8 @@
 first_number = input("First num: ")
 second_number = input("Second num: ")
 
-sum = int(first_number) + int(second_number)
-print(sum)
+addition = int(first_number) + int(second_number)
+print(addition)
 
 substarction = float(first_number) - float(second_number)
 print("Ans: " + str(substarction))
